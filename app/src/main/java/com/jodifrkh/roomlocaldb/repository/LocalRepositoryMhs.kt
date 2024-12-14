bd1049b205ff5c2dc077ef6d27d5ff2971b50139
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-15 00:46:20 +0700

    pengimplementasian fungsi deleteMhs, getAllMhs, getMhs, dan updateMhs yang dibuat di class interface RepositoryMhs

--- app/src/main/java/com/jodifrkh/roomlocaldb/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/jodifrkh/roomlocaldb/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,28 @@ package com.jodifrkh.roomlocaldb.repository
 
 import com.jodifrkh.roomlocaldb.data.dao.MahasiswaDao
 import com.jodifrkh.roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private  val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
     override  suspend fun  insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> =
+        mahasiswaDao.getAllMahasiswa()
+
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> =
+        mahasiswaDao.getMahasiswa(nim)
+
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
