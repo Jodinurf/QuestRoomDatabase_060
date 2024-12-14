3e985863ca347ebbd91e2c3a5e4e7fd9643b7a9e
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-15 00:57:51 +0700

    mendeklarasikan class MahasiswaHomeViewModel untuk mengelola state UI pada halaman home dengan mengambil semua data mahasiswa dari RepositoryMhs

--- app/src/main/java/com/jodifrkh/roomlocaldb/ui/viewModel/MahasiswaHomeViewModel.kt
+++ app/src/main/java/com/jodifrkh/roomlocaldb/ui/viewModel/MahasiswaHomeViewModel.kt
@@ -1,5 +1,52 @@
 package com.jodifrkh.roomlocaldb.ui.viewModel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jodifrkh.roomlocaldb.data.entity.Mahasiswa
+import com.jodifrkh.roomlocaldb.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class MahasiswaHomeViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+    val homeUIState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState (
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true
+            )
+        )
+
+}
 
 data class HomeUiState (
     val listMhs: List<Mahasiswa> = listOf(),
