a22891a88da17434245d22a9e4073274c184fc5a
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-15 01:09:54 +0700

    membuat class updateMhsViewModel untuk mengelola state UI pada halaman update mahasiswa. Memanggil RepositoryMhs dalam pengambilan data mahasiswa dimana NIM menjadi parameter untuk mengupdate data mahasiswa melalui route yang telah dibuat

--- app/src/main/java/com/jodifrkh/roomlocaldb/ui/viewModel/updateMhsViewModel.kt
+++ app/src/main/java/com/jodifrkh/roomlocaldb/ui/viewModel/updateMhsViewModel.kt
@@ -1,7 +1,26 @@
 package com.jodifrkh.roomlocaldb.ui.viewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.jodifrkh.roomlocaldb.data.entity.Mahasiswa
+import com.jodifrkh.roomlocaldb.repository.RepositoryMhs
+import com.jodifrkh.roomlocaldb.ui.navigation.DestinasiUpdate
 
+
+class updateMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+    var updateUIState by mutableStateOf(mhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+}
 
 fun Mahasiswa.toUIStateMhs() : mhsUIState = mhsUIState (
     mahasiswaEvent = this.toDetailUiEvent(),
