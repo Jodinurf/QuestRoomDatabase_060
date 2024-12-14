24f7db60e34c8ae741edda222da6782be41ddbe4
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-15 00:53:23 +0700

    mendeklarasikan data class untuk menampung data yang akan ditampilkan di UI

--- app/src/main/java/com/jodifrkh/roomlocaldb/ui/viewModel/MahasiswaHomeViewModel.kt
+++ app/src/main/java/com/jodifrkh/roomlocaldb/ui/viewModel/MahasiswaHomeViewModel.kt
@@ -1,2 +1,9 @@
 package com.jodifrkh.roomlocaldb.ui.viewModel
 
+
+data class HomeUiState (
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
